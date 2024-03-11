8c5b4324cfc5f866e44d465759916a36206fa281
dtest11 <fizlbq@gmail.com>
2024-03-11 21:58:48 +0800

    asio client

--- cpp_asio_example/src/main.cpp
+++ cpp_asio_example/src/main.cpp
@@ -1,15 +1,51 @@
-#include <functional>
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <array>
+#include <boost/asio.hpp>
 #include <iostream>
 
-#include "boost/asio.hpp"
-#include "printer.h"
+using boost::asio::ip::tcp;
 
-int main() {
-  boost::asio::io_context io;
-  printer p(io);
-  std::thread t([&] { io.run(); });
-  io.run();
-  t.join();
+int main(int argc, char* argv[]) {
+  try {
+    if (argc != 2) {
+      std::cerr << "Usage: client <host>" << std::endl;
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints =
+        resolver.resolve(argv[1], "daytime");
+
+    tcp::socket socket(io_context);
+    boost::asio::connect(socket, endpoints);
+
+    for (;;) {
+      std::array<char, 128> buf;
+      boost::system::error_code error;
+
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+      if (error == boost::asio::error::eof)
+        break;  // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error);  // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 }
